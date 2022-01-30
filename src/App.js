78783074b1f6129e634bb39b0717f680a13555b3
Patrice Tisserand <p.tisserand@gmail.com>
2022-01-30 17:36:26 +0100

    Now retrieve base account from local keypair file

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import './App.css';
 import { Connection, PublicKey, clusterApiUrl } from '@solana/web3.js';
 import { Program, Provider, web3 } from '@project-serum/anchor';
 import idl from './idl.json';
+import kp from './keyPair.json';
 
 
 // SystemProgram is a reference to the Solana runtime
 const { SystemProgram, Keypair } = web3;
 
-// Create a keypair for the account that will hold the data
-let baseAccount = Keypair.generate();
+const arr = Object.values(kp._keypair.secretKey);
+const secret = new Uint8Array(arr);
+const baseAccount = Keypair.fromSecretKey(secret);
 const programId = new PublicKey(idl.metadata.address);
 // use devnet network
 const network = clusterApiUrl('devnet');
